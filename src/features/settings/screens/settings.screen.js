7d6aea6541cf4f661d2a67965f36629eb3773cf4
bmla.nieling@student.han.nl <rick.niel@hotmail.com>
2023-06-02 19:01:29 +0200

    moved settings screen to seperate feature folder

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import SafeContainer from '../components/safeContainer';
+import SafeContainer from '../../components/safeContainer';
 
 export default function SettingsScreen() {
     return (
